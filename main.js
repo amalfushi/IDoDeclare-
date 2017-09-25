006b5e7caf1fef545f905b1d22064d30ec3cc0dc
Dustin Schroeder <dustin.p.schroeder@gmail.com>
2017-09-25 14:53:14 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var number1 = 0;
+var number2 = -37;
+var number3 = 10000;
+var number4 = 400.43;
+var string1 = "Taco";
+var string2 = "Deer";
+var string3 = "JavaScript";
+var boolean1 = true;
+var boolean2 = false;
+var a;
+
+console.log("The First Number: ", number1);
+console.log("The Second Number: ", number2);
+console.log("The Third Number: ", number3);
+console.log("The Fourth Number: ", number4);
+console.log("The First String: ", string1);
+console.log("The Second String: ", string2);
+console.log("The Third String: ", string3);
+console.log("The First Boolean: ", boolean1);
+console.log("The Second Boolean: ", boolean2);
+console.log("The Undefined Variable: ", a);
+
